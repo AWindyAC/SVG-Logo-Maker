316d41654e3407bcb0c04e0e4ae4e09d48ce835a
Wendell Fowlkes <fowlkes.wendell@gmail.com>
2023-07-01 11:30:30 -0400

    Updated code, doesn't function

--- index.js
+++ index.js
@@ -1,48 +1,61 @@
 const inquirer = require('inquire');
 const fs = require('fs');
-const generateLogo = require('./lib/generateLogo');
-
-inquirer
-.prompt([
-    {
-        type: 'input',
-        message: 'What text would you like you in your logo?',
-        name: 'text',
-    },
-    {
-        type: 'input',
-        message: 'What color would you like the text to be?',
-        name: 'text color',
-    },
-    {
-        type: 'list',
-        message: 'What shape would you like you logo to be?',
-        name: 'shape',
-        choices: ['Triangle', 'Square', 'Circle'],
-    },
-    {
-        type: 'input',
-        message: 'What color would you like the shape to be?',
-        name: 'shape color',
-    }
-])
-
-function writeToFile(fileName, data)
-{
-    const content = generateLogo(data);
-    fs.writeFile(fileName, content, function(err) {
-        if (err) {
-            console.log(err);
-        }
-        console.log('Generated logo.svg');
-    });
-}
-
-function init(){
-    inquirer.prompt(questions).then(function(data){
-        var fileName = 'logo.svg';
-        writeToFile(fileName, data);
-    });
-}
+const SVG = require('./lib/svg');
+const { Circle, Triangle, Square } = require('./lib/shapes');
+const { writeFile } = require('fs/promises'); //Getting access to use writeFile from the file system.
 
 init();
+
+//List of questions in the inquirer
+function init(){
+    inquirer
+    .prompt([
+        {
+            type: 'input',
+            message: 'What text would you like you in your logo?', //logo text
+            name: 'text',
+        },
+        {
+            type: 'input',
+            message: 'What color would you like the text to be?', //text color
+            name: 'textColor',
+        },
+        {
+            type: 'list',
+            message: 'What shape would you like you logo to be?', //logo shape
+            name: 'shapeType',
+            choices: ['Triangle', 'Square', 'Circle'],
+        },
+        {
+            type: 'input',
+            message: 'What color would you like the shape to be?', //shape color
+            name: 'shapeColor',
+        }
+    ])
+    //What happens after the questions are answered.
+    .then(({text, textColor, shapeType, shapeColor}) => {
+        let Shape;
+        //Check 
+        switch (shapeType) {
+            case "Triangle":
+                Shape = new Triangle();
+                break;
+            case "Square":
+                Shape = new Square();
+                break;
+            default :
+                Shape= new Circle();
+                break;
+            };
+        shape.setColor(shapeColor);
+        //Create a new SVG object using the information user has given.
+        const svg = new SVG(); //new SVG object.
+        svg.setText(text, textColor); //use setText method created in svg.js 
+        svg.setShape(shape); //use setShape method created in svg.js
+        return writeFile('logo.svg', svg.render()); //use writeFile to create a new svg file
+    })
+    //if an error occurs, print this to the console. 
+    .catch((error) => {
+        console.log("Error:", error);
+    })
+}
